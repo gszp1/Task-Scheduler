a749d24c5c46456350e46b4143eeaa49199719d4
gszp1 <grzybekmateusz@poczta.fm>
2024-01-04 23:28:03 +0100

    Adds includes for needed libraries.

--- task_scheduler.h
+++ task_scheduler.h
@@ -1,4 +1,9 @@
 #ifndef TASK_SCHEDULER_TASK_SCHEDULER_H
 #define TASK_SCHEDULER_TASK_SCHEDULER_H
 
+// Includes //
+#include <stdio.h>
+#include <stdlib.h>
+#include <mqueue.h>
+
 #endif //TASK_SCHEDULER_TASK_SCHEDULER_H
