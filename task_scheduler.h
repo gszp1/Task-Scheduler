2f6146907308aac1093e5bad70e8d4cf2186a06a
gszp1 <grzybekmateusz@poczta.fm>
2024-01-04 23:29:33 +0100

    Adds macro for server queue name.

--- task_scheduler.h
+++ task_scheduler.h
@@ -1,6 +1,9 @@
 #ifndef TASK_SCHEDULER_TASK_SCHEDULER_H
 #define TASK_SCHEDULER_TASK_SCHEDULER_H
 
+// Macros //
+#define SERVER_QUEUE_NAME "server_queue"
+
 // Includes //
 #include <stdio.h>
 #include <stdlib.h>
