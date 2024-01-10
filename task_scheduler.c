f140be1248591c2e1fef73344cbb8264bd15118b
gszp1 <grzybekmateusz@poczta.fm>
2024-01-10 04:07:42 +0100

    Adds program arguments to main signature.

--- task_scheduler.c
+++ task_scheduler.c
@@ -1,6 +1,6 @@
 #include "task_scheduler.h"
 
-int main() {
+int main(int argc, char* argv[]) {
     //try to create main queue
     mqd_t server_msg_queue = mq_open(SERVER_QUEUE_NAME, O_CREAT | O_EXCL | O_RDWR, 0666, NULL);
     //program flow - true -> client | false -> server
