8a8d13807cbb9173e3742ed270b4539391d99e2c
gszp1 <grzybekmateusz@poczta.fm>
2024-01-10 04:17:50 +0100

    Adds server_msg_queue_attributes.

--- task_scheduler.c
+++ task_scheduler.c
@@ -1,8 +1,13 @@
 #include "task_scheduler.h"
 
 int main(int argc, char* argv[]) {
+    
+    struct mq_attr server_msg_queue_attributes;
+    server_msg_queue_attributes.mq_maxmsg = MAX_MESSAGES;
+    server_msg_queue_attributes.mq_flags = 0;
+
     //try to create main queue
-    mqd_t server_msg_queue = mq_open(SERVER_QUEUE_NAME, O_CREAT | O_EXCL | O_RDWR, 0666, NULL);
+    mqd_t server_msg_queue = mq_open(SERVER_QUEUE_NAME, O_CREAT | O_EXCL | O_RDWR, 0666, &server_msg_queue_attributes);
     //program flow - true -> client | false -> server
     if ((server_msg_queue == -1) && (errno == EEXIST)) {
         
