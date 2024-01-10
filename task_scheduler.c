4846b54ca5fd2a10fc63ec07794b682e614002d9
gszp1 <grzybekmateusz@poczta.fm>
2024-01-10 04:19:41 +0100

    Sets temporarly msgsize.

--- task_scheduler.c
+++ task_scheduler.c
@@ -5,6 +5,7 @@ int main(int argc, char* argv[]) {
     struct mq_attr server_msg_queue_attributes;
     server_msg_queue_attributes.mq_maxmsg = MAX_MESSAGES;
     server_msg_queue_attributes.mq_flags = 0;
+    server_msg_queue_attributes.mq_msgsize = 100 * sizeof(char); //temporary
 
     //try to create main queue
     mqd_t server_msg_queue = mq_open(SERVER_QUEUE_NAME, O_CREAT | O_EXCL | O_RDWR, 0666, &server_msg_queue_attributes);
