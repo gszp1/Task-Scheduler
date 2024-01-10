1ea98231b56cd175e10006d9865b662754dd2427
gszp1 <grzybekmateusz@poczta.fm>
2024-01-10 04:03:54 +0100

    Changes server_queue to server_msg_queue.

--- task_scheduler.c
+++ task_scheduler.c
@@ -2,12 +2,12 @@
 
 int main() {
     //try to create main queue
-    mqd_t server_queue = mq_open(SERVER_QUEUE_NAME, O_CREAT | O_EXCL | O_RDWR, 0666, NULL);
+    mqd_t server_msg_queue = mq_open(SERVER_QUEUE_NAME, O_CREAT | O_EXCL | O_RDWR, 0666, NULL);
     //program flow - true -> client | false -> server
-    if ((server_queue == -1) && (errno == EEXIST)) {
+    if ((server_msg_queue == -1) && (errno == EEXIST)) {
         
     } else {
-        
+
     }
     
 }
