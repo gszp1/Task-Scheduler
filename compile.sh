63c83b38743bea6acb12fd25b14c22522848720a
gszp1 <grzybekmateusz@poczta.fm>
2024-01-14 11:40:14 +0100

    Adds notifications about setting scripts permitions.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# Don't forget to add execute permission: sudo chmod +x compile.sh
+
 gcc task_scheduler.c task_scheduler.h -o task_scheduler_out
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Don't forget to add execute permission: sudo chmod +x task_scheduler_out
+# Don't forget to add execute permission: sudo chmod +x run.sh
 
 ./task_scheduler_out
