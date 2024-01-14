deb285ee21cea23a4e8064bea730828fd9c6b956
gszp1 <grzybekmateusz@poczta.fm>
2024-01-14 12:02:49 +0100

    Adds reading program arguments.

--- task_scheduler.c
+++ task_scheduler.c
@@ -11,6 +11,10 @@ int main(int argc, char* argv[]) {
     mqd_t server_msg_queue = mq_open(SERVER_QUEUE_NAME, O_CREAT | O_EXCL | O_RDONLY, 0666, &server_msg_queue_attributes);
     if ((server_msg_queue == -1) && (errno == EEXIST)) {
         printf("Running process as client.");
+
+        for (int i = 0; i < argc; ++i) {
+            printf("%s\n", argv[i]);
+        }
         server_msg_queue  = mq_open(SERVER_QUEUE_NAME, O_WRONLY, 0666, &server_msg_queue_attributes);
         if (server_msg_queue == -1) {
             return -1;
@@ -18,7 +22,7 @@ int main(int argc, char* argv[]) {
         mq_close(server_msg_queue);
     } else {
         printf("Running process as server.");
-        while (getchar() != 'q');
+        while (getchar() != 'q'); // temporary, change to proper requests handling later
         mq_close(server_msg_queue);
         mq_unlink(SERVER_QUEUE_NAME);
     }
