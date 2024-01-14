e4ee4f2d260a346366221ad0610e0589d2a82551
gszp1 <grzybekmateusz@poczta.fm>
2024-01-14 12:35:04 +0100

    Adds -lrt to run.sh in order to link against real-time library (librt).

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@
 
 # Don't forget to add execute permission: sudo chmod +x compile.sh
 
-gcc task_scheduler.c task_scheduler.h -o task_scheduler_out
+gcc task_scheduler.c task_scheduler.h -lrt -o task_scheduler_out
