293591b12e4ccb1c5814b02a9999832e88616b00
gszp1 <grzybekmateusz@poczta.fm>
2024-01-14 12:22:55 +0100

    Changes permissions to main queue to : read-only for server, write-only to client.

--- task_scheduler.c
+++ task_scheduler.c
@@ -1,21 +1,20 @@
 #include "task_scheduler.h"
 
 int main(int argc, char* argv[]) {
-    
     struct mq_attr server_msg_queue_attributes;
     server_msg_queue_attributes.mq_maxmsg = MAX_MESSAGES;
     server_msg_queue_attributes.mq_flags = 0;
     server_msg_queue_attributes.mq_msgsize = 100 * sizeof(char); //temporary
 
     //try to create main queue
-    mqd_t server_msg_queue = mq_open(SERVER_QUEUE_NAME, O_CREAT | O_EXCL | O_RDONLY, 0666, &server_msg_queue_attributes);
+    mqd_t server_msg_queue = mq_open(SERVER_QUEUE_NAME, O_CREAT | O_EXCL | O_RDONLY, 0444, &server_msg_queue_attributes);
     if ((server_msg_queue == -1) && (errno == EEXIST)) {
         printf("Running process as client.");
-
+        // temporary reading and printing program arguments.
         for (int i = 0; i < argc; ++i) {
             printf("%s\n", argv[i]);
         }
-        server_msg_queue  = mq_open(SERVER_QUEUE_NAME, O_WRONLY, 0666, &server_msg_queue_attributes);
+        server_msg_queue  = mq_open(SERVER_QUEUE_NAME, O_WRONLY, 0222, &server_msg_queue_attributes);
         if (server_msg_queue == -1) {
             return -1;
         }
@@ -26,5 +25,5 @@ int main(int argc, char* argv[]) {
         mq_close(server_msg_queue);
         mq_unlink(SERVER_QUEUE_NAME);
     }
-    
+
 }
